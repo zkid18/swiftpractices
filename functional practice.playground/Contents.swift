4f104a7d30a3b044b3cddc110e56096477e4e1a4
your name <your@email.com>
2017-01-16 14:22:39 +0300

    Edited functional programming practice

--- functional practice.playground/Contents.swift
+++ functional practice.playground/Contents.swift
@@ -2,10 +2,10 @@
 
 import UIKit
 
-var str = "Hello, playground"
-
 
 typealias Distance = Double
+
+//the Region type will refer to functions transforming a Position to a Bool
 typealias Region = Position -> Bool
 
 struct Position {
@@ -13,6 +13,8 @@ struct Position {
     var y: Double
 }
 
+
+//check wether point is in circle
 extension Position {
     func inRange(range: Distance) -> Bool {
         return sqrt(x*x + y*y) <= range
@@ -20,13 +22,15 @@ extension Position {
     
 }
 
-
+// in case if ship has other position, than origin
 struct Ship {
     var position: Position
     var firingRange: Distance
     var unsafeRange: Distance
 }
 
+
+//Test if another ship is on range
 extension Ship {
     func canEngageShip(target: Ship) -> Bool {
         let dx = target.position.x - position.x
@@ -37,6 +41,8 @@ extension Ship {
     }
 }
 
+
+//For avoiding targeting ships if they are too close to you
 extension Ship {
     func canSafelyEngagePosition(target: Ship) -> Bool {
         let dx = target.position.x - position.x
@@ -46,6 +52,8 @@ extension Ship {
     }
 }
 
+
+//For avoiding tarheting ships if they are to close to each other
 extension Ship {
     func canSafelyEngageShip1(target:Ship, friendly: Ship) -> Bool {
         let dx = target.position.x - position.x
@@ -59,6 +67,7 @@ extension Ship {
     }
 }
 
+//to maintatin the complexity of code, make methods for static calculation
 extension Position {
     func minus(p: Position) -> Position {
         return Position(x: p.x, y: p.y)
@@ -77,12 +86,15 @@ extension Ship {
     }
 }
 
+//MARK: Refactoring
 
+//circules, centered around origin
 func circle(radius: Distance) -> Region  {
     return { point in point.length <= radius }
 }
 
 
+//circules, shifting from center
 func circle2(radius: Distance, center: Position) -> Region {
     return {point in point.minus(center).length <= radius }
 }
@@ -92,7 +104,7 @@ func shift(region: Region, offset: Position) -> Region {
     return { point in region(point.minus(offset))}
 }
 
-
+//method for defining a new region by inventing a region
 func invent(region: Region) -> Region {
         return {point in !region(point)}
 }
@@ -108,4 +120,15 @@ func union(region1: Region, _ region2: Region) -> Region {
 
 func difference (region: Region, minus: Region) -> Region {
     return intersection(region, invent(region))
+}
+
+//refactored method
+extension Ship {
+    func canSafelyEngageShip(target: Ship, friendly: Ship) -> Bool {
+        let rangeRegion = difference(circle(firingRange), minus: circle(unsafeRange))
+        let firingRegion = shift(rangeRegion, offset: position)
+        let friendlyRegion = shift(circle(unsafeRange), offset: friendly.position)
+        let resultRegion = difference(firingRegion, minus: friendlyRegion)
+        return resultRegion(target.position)
+    }
 }
